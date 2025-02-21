2198bc3874931ddb9c53b5d9c7ac21bb5be809cc
Marcelo Tieghi <tieghi77@gmail.com>
2025-02-21 23:12:29 +0000

    Initialize Infra layer with Entity Framework context

---
+++ src/TieghiCorp.Infra/Data/Context/Mappings/LocationDbConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TieghiCorp.Core.Entities;
+
+namespace TieghiCorp.Infra.Data.Context.Mappings;
+
+internal sealed class LocationDbConfig : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.BaseEntityConfigure();
+
+        builder.ToTable("Location");
+
+        builder
+            .Property(l => l.Name)
+            .HasColumnType("varchar")
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder
+            .HasIndex(l => l.Name)
+            .IsUnique();
+
+        builder
+            .HasMany<Department>()
+            .WithOne(d => d.Location)
+            .HasForeignKey(d => d.LocationId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ src/TieghiCorp.Infra/InfraServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TieghiCorp.Infra.Data.Context;
+
+namespace TieghiCorp.Infra;
+
+public static class InfraServices
+{
+    public static void AddInfraServices(
+        this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddDbContext<AppDbContext>(
+            opt => opt.UseSqlServer(config.GetConnectionString("DefaultConn")));
+    }
+}
