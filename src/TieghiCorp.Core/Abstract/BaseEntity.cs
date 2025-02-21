7e58b746805e1d28023e06a9a5afe07ad01a301e
Marcelo Tieghi <tieghi77@gmail.com>
2025-02-21 22:55:14 +0000

    Initial structure of the Core layer

---
+++ src/TieghiCorp.Core/Abstract/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿namespace TieghiCorp.Core.Abstract;
+
+public abstract record BaseEntity
+{
+    public int Id { get; set; }
+
+    public void UpdateEntity(object updatedValues)
+    {
+        if (updatedValues == null)
+            return;
+
+        var updatedProperties = updatedValues.GetType().GetProperties();
+        var entityType = this.GetType();
+
+        foreach (var updatedProperty in updatedProperties)
+        {
+            var entityProperty = entityType.GetProperty(updatedProperty.Name);
+
+            if (entityProperty != null && entityProperty.CanWrite)
+            {
+                var newValue = updatedProperty.GetValue(updatedValues);
+                if (newValue != null)
+                {
+                    entityProperty.SetValue(this, Convert.ChangeType(newValue, entityProperty.PropertyType));
+                }
+            }
+        }
+    }
+}
