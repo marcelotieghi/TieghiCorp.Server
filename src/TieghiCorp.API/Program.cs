956175eca332f155ebb77e3ba550cccd89abb35e
Marcelo Tieghi <tieghi77@gmail.com>
2025-02-22 12:11:44 +0000

    Add services conteiner UseCases layer

--- src/TieghiCorp.API/Program.cs
+++ src/TieghiCorp.API/Program.cs
@@ -1,8 +1,10 @@
 using TieghiCorp.Infra;
+using TieghiCorp.UseCases;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfraServices(builder.Configuration);
+builder.Services.AddUseCasesServices();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
