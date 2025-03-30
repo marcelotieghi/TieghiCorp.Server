e0a9df0b701144456fbcb539c76c1be5ce4fccc1
Marcelo Tieghi <tieghi77@gmail.com>
2025-03-30 02:45:59 +0100

    Fixing list response

--- src/TieghiCorp.UseCases/Personnel/GetAll/GetAllPersonnelHandler.cs
+++ src/TieghiCorp.UseCases/Personnel/GetAll/GetAllPersonnelHandler.cs
@@ -16,22 +16,37 @@ internal sealed class GetAllPersonnelHandler(
         var personnel = _personnelQuery.GetAll();
 
         if (!string.IsNullOrEmpty(request.SearchTerm))
-            personnel = personnel.Where(d => d.Email.Contains(request.SearchTerm, StringComparison.OrdinalIgnoreCase));
+        {
+            personnel = personnel.Where(d =>
+                EF.Functions.Like(d.FirstName.ToLower(), $"%{request.SearchTerm.ToLower()}%") ||
+                EF.Functions.Like(d.LastName.ToLower(), $"%{request.SearchTerm.ToLower()}%") ||
+                EF.Functions.Like(d.Email.ToLower(), $"%{request.SearchTerm.ToLower()}%") ||
+                EF.Functions.Like(d.JobTitle.ToLower(), $"%{request.SearchTerm.ToLower()}%") ||
+                EF.Functions.Like(d.Department!.Name.ToLower(), $"%{request.SearchTerm.ToLower()}%"));
+        }
 
         personnel = request.SortField.ToLower() switch
         {
             "firstName" => request.SortDirection.ToLower() == "asc"
                 ? personnel.OrderBy(d => d.FirstName)
                 : personnel.OrderByDescending(d => d.FirstName),
+
             "lastname" => request.SortDirection.ToLower() == "asc"
                 ? personnel.OrderBy(d => d.LastName)
                 : personnel.OrderByDescending(d => d.LastName),
+
             "email" => request.SortDirection.ToLower() == "asc"
                 ? personnel.OrderBy(d => d.Email)
                 : personnel.OrderByDescending(d => d.Email),
+
             "jobtitle" => request.SortDirection.ToLower() == "asc"
                 ? personnel.OrderBy(d => d.JobTitle)
                 : personnel.OrderByDescending(d => d.JobTitle),
+
+            "departmentname" => request.SortDirection.Equals("asc", StringComparison.CurrentCultureIgnoreCase)
+                ? personnel.OrderBy(p => p.Department!.Name)
+                : personnel.OrderByDescending(p => p.Department!.Name),
+
             _ => request.SortDirection.ToLower() == "asc"
                 ? personnel.OrderBy(d => d.Id)
                 : personnel.OrderByDescending(d => d.Id)
